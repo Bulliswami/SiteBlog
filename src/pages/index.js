991f1d7247a0fa45807069ad4779e63592cf3afb
swami <swamireddy42@gmail.com>
2023-09-21 11:14:43 -0400

    code updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Link from "gatsby-link";
-
+import "./index.css";
 // Basic boilerplate code for content to display
 export default () => (
     <div>
-        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <h1>Hi! I'm building a fake Gatsby site as part of my learning!</h1>
         <p>
-            What do I like to do? Lots of course but definitely enjoy building
+            What do I like to do? Lots of coding but definitely enjoy building
             websites.
         </p>
         <Link to="/blog">Go to blog</Link>
+        <footer className="footer"> © 2021 Copyright:Bulli Swami Reddy Goluguri</footer>
     </div>
 );
